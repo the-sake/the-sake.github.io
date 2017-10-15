9c5e96928be4e5885891113dc44786009be650d3
Gim YoungPhil <youngphil.gim@gmail.com>
2017-10-16 01:47:26 +0900

    fixed Jumbo

--- src/components/classification/Jombotron.vue
+++ src/components/classification/Jombotron.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="jumbotron">
     <h1 class="display-4">사케의 분류</h1>
-    <p class="lead">
+    <hr class="my-4">
+    <p class="">
       특정명칭주
       <ul>
-        <li><span class="text-primary">준마이 타입</span>: 쌀, 쌀누룩</li>
-        <li><span class="text-danger">혼죠조 타입</span>: 쌀, 쌀누룩, 규정량 내의 양조알코올</li>
+        <li><u class="text-primary">준마이 타입</u>: 쌀, 쌀누룩</li>
+        <li><u class="text-danger">혼죠조 타입</u>: 쌀, 쌀누룩 / 규정량 내의 양조알코올</li>
       </ul>
-    </p>
-    <p class="lead">
       보통주
       <ul>
-        <li><span class="text-success">후츠슈</span>: 쌀, 쌀누룩, 규정량 내의 양조알코올, 그 외의 원재료</li>
+        <li><u class="text-success">후츠슈 타입</u>: 쌀, 쌀누룩 / 규정량 내의 양조알코올 / 그 외의 원재료</li>
       </ul>
     </p>
   </div>
