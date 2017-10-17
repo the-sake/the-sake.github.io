fe2b391f954724319412de2fdc8b5d85dcb692d6
Gim YoungPhil <youngphil.gim@gmail.com>
2017-10-17 13:17:58 +0900

    add copy

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -91,6 +92,9 @@ if (process.env.NODE_ENV === 'production') {
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
-    })
+    }),
+    new CopyWebpackPlugin([
+      { from: 'data/images', to: 'images' }
+    ])
   ])
 }
