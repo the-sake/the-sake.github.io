3ce9a69e302ca516d207934823759a427a36c56e
Gim YoungPhil <youngphil.gim@gmail.com>
2017-10-31 23:41:40 +0900

    fixed _blank

--- src/components/app/Footer.vue
+++ src/components/app/Footer.vue
@@ -3,16 +3,16 @@
     <div class="container">
       <p>Colophon</p>
       <p>
-        Built with <a href="https://vuejs.org/" target="_blank">Vue.js</a>,
-        <a href="https://getbootstrap.com/" target="_blank">Bootstrap</a> and
-        served on <a href="https://github.com/the-sake" target="_blank">GitHub</a>.
+        Built with <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue.js</a>,
+        <a href="https://getbootstrap.com/" target="_blank" rel="noopener">Bootstrap</a> and
+        served on <a href="https://github.com/the-sake" target="_blank" rel="noopener">GitHub</a>.
         <br/>
         Text is set in Google Font’s
-        <a href="https://googlefonts.github.io/japanese/" target="_blank">Japanese Early Access</a>
+        <a href="https://googlefonts.github.io/japanese/" target="_blank" rel="noopener">Japanese Early Access</a>
       </p>
       <p>
         <em>Copyright © 2017</em>
-        <a href="https://github.com/GimYoungPhil" target="_blank">YoungPhil</a>
+        <a href="https://github.com/GimYoungPhil" target="_blank" rel="noopener">YoungPhil</a>
       </p>
     </div>
   </div>
